6652967b2c680516726e3840293a6797e14edf77
YuikoIto <toffywing@gmail.com>
2020-07-09 00:39:30 +0900

    make better

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'あなたの好きな色はなんですか？'
+      'あなたの好きな動物はなんですか？']
     return MaterialApp(
-      home: Text('Hello'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quiz App'),
+        ),
+        body: Column(children: [
+          Text('The question'),
+          RaisedButton(
+            child: Text('answer1'),
+            onPressed: null
+          ),
+          RaisedButton(
+            child: Text('answer2'),
+            onPressed: null
+          ),
+          RaisedButton(
+            child: Text('answer3'),
+            onPressed: null
+          ),  
+        ],),
+      ),
     );
   }
 }
