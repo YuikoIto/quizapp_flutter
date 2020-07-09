da335920e0699c076cdd3ab507cf0296e9d12cd2
YuikoIto <toffywing@gmail.com>
2020-07-10 03:30:04 +0900

    make pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,57 @@
 import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  var questionIndex = 0;
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
 
-  void answerQuestion() {
-    questionIndex += 1;
-    print(questionIndex);
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex += 1;
+    });
+
+    print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = ['あなたの好きな色はなんですか？', 'あなたの好きな動物はなんですか？'];
+    var questions = [
+      {
+        'questionText': 'あなたの好きな色はなんですか？',
+        'answers': ['黒', '赤', '緑', '白'],
+      },
+      {
+        'questionText': 'あなたは何型ですか？',
+        'answers': ['A', 'B', 'AB', 'O'],
+      },
+      {
+        'questionText': 'あなたの性格は？',
+        'answers': ['穏やか', '激しい', '冷静', '情緒不安定'],
+      },
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Quiz App'),
+          title: Text('性格診断'),
         ),
         body: Column(
           children: [
-            Text(
-              questions[questionIndex],
+            Question(
+              questions[_questionIndex]['questionText'],
             ),
-            RaisedButton(
-              child: Text('answer1'),
-              onPressed: answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('answer2'),
-              onPressed: () => print('answer2'),
-            ),
-            RaisedButton(
-              child: Text('answer3'),
-              onPressed: () {
-                print('answer3');
-              },
-            ),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
         ),
       ),
