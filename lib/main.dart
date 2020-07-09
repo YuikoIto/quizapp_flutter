52798cefb235dce2876effecedca6cebf291467b
YuikoIto <toffywing@gmail.com>
2020-07-09 22:06:56 +0900

    change layout

--- lib/main.dart
+++ lib/main.dart
@@ -3,31 +3,42 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    questionIndex += 1;
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'あなたの好きな色はなんですか？'
-      'あなたの好きな動物はなんですか？']
+    var questions = ['あなたの好きな色はなんですか？', 'あなたの好きな動物はなんですか？'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Quiz App'),
         ),
-        body: Column(children: [
-          Text('The question'),
-          RaisedButton(
-            child: Text('answer1'),
-            onPressed: null
-          ),
-          RaisedButton(
-            child: Text('answer2'),
-            onPressed: null
-          ),
-          RaisedButton(
-            child: Text('answer3'),
-            onPressed: null
-          ),  
-        ],),
+        body: Column(
+          children: [
+            Text(
+              questions[questionIndex],
+            ),
+            RaisedButton(
+              child: Text('answer1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('answer2'),
+              onPressed: () => print('answer2'),
+            ),
+            RaisedButton(
+              child: Text('answer3'),
+              onPressed: () {
+                print('answer3');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
